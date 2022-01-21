1c45c0ba05d0274c59108c2a3b1ac9b6360b1c55
gbaquero06 <gbaquerolandazuri@newtech.coppellisd.com>
2022-01-21 13:48:59 -0600

    FINAL2

--- src/lab/Runner.java
+++ src/lab/Runner.java
@@ -24,7 +24,7 @@ public class Runner {
 		houses = new ArrayList<House>();
 
 		Scanner inFile = new Scanner(new File("src/lab/info.txt"));
-		//adds the information to the array list
+		// adds the information to the array list
 		while (inFile.hasNext()) {
 			int sqfoot = inFile.nextInt();
 			double bath = inFile.nextDouble();
@@ -36,9 +36,9 @@ public class Runner {
 			boolean pool = inFile.nextBoolean();
 			String image = inFile.next();
 			houses.add(new House(sqfoot, bath, bedroom, price, address, lot, pool, image));
-			
+
 		}
-		
+
 		MainPage();
 	}
 
@@ -49,7 +49,6 @@ public class Runner {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// end program when red x is hit
 		// give it size
 		frame.setSize(700, 700);
-		// everythig else goes here
 
 		// create pic object
 		ImageIcon pic1 = new ImageIcon(houses.get(0).image());
@@ -58,18 +57,19 @@ public class Runner {
 
 		JButton house1 = new JButton(scaledPic1);
 		house1.setBounds(20, 30, scaledPic1.getIconWidth(), scaledPic1.getIconHeight());
-		frame.add(house1);
+		frame.add(house1);// add pic of house 1 to frame
 		JLabel LabelHouse1 = new JLabel(houses.get(0).address());
 		LabelHouse1.setBounds(20, 180, 240, 30);// x,y.with,heigh
-		frame.add(LabelHouse1);
+		frame.add(LabelHouse1);// add address of house 1 to the frame
 
 		house1.addActionListener(new ActionListener() {
-
+			//waits for user to click the pic
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				frame.setVisible(false);
-
+				//when pic is clicked closes the main page
 				houseGUI(houses.get(0));
+				// opens the house page
 			}
 		});
 
@@ -78,10 +78,10 @@ public class Runner {
 
 		JButton house2 = new JButton(scaledPic2);
 		house2.setBounds(20, 220, scaledPic2.getIconWidth(), scaledPic2.getIconHeight());
-		frame.add(house2);
+		frame.add(house2);// add pic of house 2 to frame
 		JLabel LabelHouse2 = new JLabel(houses.get(1).address());
 		LabelHouse2.setBounds(20, 370, 240, 30);// x,y.with,heigh
-		frame.add(LabelHouse2);
+		frame.add(LabelHouse2);// add address of house 2 to the frame
 
 		house2.addActionListener(new ActionListener() {
 
@@ -97,10 +97,10 @@ public class Runner {
 
 		JButton house3 = new JButton(scaledPic3);
 		house3.setBounds(20, 410, scaledPic3.getIconWidth(), scaledPic3.getIconHeight());
-		frame.add(house3);
+		frame.add(house3); // add pic of house 3 to frame
 		JLabel LabelHouse3 = new JLabel(houses.get(2).address());
 		LabelHouse3.setBounds(20, 560, 300, 30);// x,y.with,heigh
-		frame.add(LabelHouse3);
+		frame.add(LabelHouse3);// add address of house 3 to the frame
 
 		house3.addActionListener(new ActionListener() {
 
@@ -116,10 +116,10 @@ public class Runner {
 
 		JButton house4 = new JButton(scaledPic4);
 		house4.setBounds(350, 30, scaledPic4.getIconWidth(), scaledPic3.getIconHeight());
-		frame.add(house4);
+		frame.add(house4);// add pic of house 4 to frame
 		JLabel LabelHouse4 = new JLabel(houses.get(3).address());
 		LabelHouse4.setBounds(350, 180, 240, 30);// x,y.with,heigh
-		frame.add(LabelHouse4);
+		frame.add(LabelHouse4);// add address of house 4 to the frame
 
 		house4.addActionListener(new ActionListener() {
 
@@ -135,10 +135,10 @@ public class Runner {
 
 		JButton house5 = new JButton(scaledPic5);
 		house5.setBounds(350, 220, scaledPic5.getIconWidth(), scaledPic5.getIconHeight());
-		frame.add(house5);
+		frame.add(house5);// add pic of house 5 to frame
 		JLabel LabelHouse5 = new JLabel(houses.get(4).address());
 		LabelHouse5.setBounds(350, 370, 300, 30);// x,y.with,heigh
-		frame.add(LabelHouse5);
+		frame.add(LabelHouse5); // add address of house 5 to the frame
 
 		house5.addActionListener(new ActionListener() {
 
@@ -154,10 +154,10 @@ public class Runner {
 
 		JButton house6 = new JButton(scaledPic6);
 		house6.setBounds(350, 410, scaledPic6.getIconWidth(), scaledPic6.getIconHeight());
-		frame.add(house6);
+		frame.add(house6);// add pic of house 6 to frame
 		JLabel LabelHouse6 = new JLabel(houses.get(5).address());
 		LabelHouse6.setBounds(350, 560, 300, 30);// x,y.with,heigh
-		frame.add(LabelHouse6);
+		frame.add(LabelHouse6); // add address of house 6 to the frame
 
 		house6.addActionListener(new ActionListener() {
 
@@ -184,21 +184,23 @@ public class Runner {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		frame.setSize(700, 700);
+		// builds the house frame
 		JButton goback = new JButton("Go back to home page");
 		goback.setBounds(10, 10, 200, 30);// x,y.with,heigh
-		frame.add(goback);
+		frame.add(goback);// add a go back button
 		goback.addActionListener(new ActionListener() {
-
+			// waits for user to click the button
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				frame.setVisible(false);
 				MainPage();
+				// goes back to the main page
 			}
 		});
 		ImageIcon pic1 = new ImageIcon(house.image());
 		// scale pic object
 		ImageIcon scaledPic1 = new ImageIcon(pic1.getImage().getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
-
+		//add the information to the frame
 		JButton house1 = new JButton(scaledPic1);
 		house1.setBounds(20, 50, scaledPic1.getIconWidth(), scaledPic1.getIconHeight());
 		frame.add(house1);
@@ -227,6 +229,7 @@ public class Runner {
 		JLabel lot = new JLabel("Lot size:      " + house.lot());
 		lot.setBounds(300, 140, 300, 30);// x,y.with,heigh
 		frame.add(lot);
+		// checks if there is a pool to add the correct information to the frame
 		if (house.pool() == true) {
 			JLabel pool = new JLabel("Has pool:      Yes");
 			pool.setBounds(300, 160, 300, 30);// x,y.with,heigh
